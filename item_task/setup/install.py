2f681113d10a040dc8182722e90521b71b6a6b8b
Kowsalya S <kowsi2211>
2025-08-01 16:35:32 +0530

    value change

--- item_task/setup/install.py
+++ item_task/setup/install.py
@@ -6,7 +6,6 @@ def before_install():
         doc.item_code = "Chocolate"
         doc.item_name = "Chocolate"
         doc.item_group = "Consumable"
-        doc.stock_uom = "Nos"
         doc.is_stock_item = 1
         doc.insert(ignore_permissions=True)
         frappe.msgprint(f"Item '{doc.item_code}' is created.")
